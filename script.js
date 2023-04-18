d5c876e649a6480edd337ef84d782c50dbb5e3db
ClaraHeadstron <91910392+ClaraHeadstron@users.noreply.github.com>
2023-04-18 16:01:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+    function factfileFunction(myPopup) {
+        var popup = document.getElementById("myPopup");
+        popup.classList.toggle("show");
+    }
+    
+    function factfileExitFunction(myPopup) {
+        var popup = document.getElementById("myPopup");
+        popup.classList.toggle("hide");
+    }
+</script>
